0dda1013e37abb3149ce273bcdba6f867a2df78e
govinda8668 <govindalokhande3@gmail.com>
2024-04-04 09:13:53 +0530

    change

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from "express";
 
 const app =express();
-
+app.use(express.json());
 const PORT =5000;
 
 app.get("/order/food", (req, res) => {
 const {menu, price,quantity} =req.query;
 
+const {user, country}= req.headers;
    const totalPrice = parseInt(price) * parseInt(quantity);
   
     res.json({
         message: `You have ordered ${quantity} ${menu}`,
-        bill:`Your total bill is ${totalPrice}`
+        bill:`Your total bill is ${totalPrice}`,
+        details:` You are ${user} from ${country}`
     })
 })
 
@@ -32,6 +34,14 @@ else{
         message: "You have ordered food"
     })
 }
+})
+
+app.post('/user', (req, res)=>{
+const{name, age} = req.body;
+
+res.json ({
+    message:`Hello ${name}, you are ${age} years old`
+})
 })
 app.listen(PORT, ()=>{
 
